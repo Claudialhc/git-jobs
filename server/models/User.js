4f7c9009922fedcaf4cf3c404db5b4d15a67ab77
Timothy Smith <timothytsmith8@gmail.com>
2021-02-26 15:15:56 -0800

    Update models.

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcrypt-nodejs");
 
+const FavoriteSchema = new Schema({
+  title: { type: String, required: true },
+  type: { type: String, required: true },
+  url: { type: String, required: true },
+  created: { type: String, required: true },
+  company: { type: String, required: true },
+  companyUrl: { type: String, required: true },
+  location: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  apply: { type: String, required: true },
+  logo: { type: String, required: true },
+  id: { type: Number, required: true }
+});
+
 const UserSchema = new Schema({
   username: {
     type: String,
@@ -30,7 +45,12 @@ const UserSchema = new Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
+  favorites: [
+    {
+      type: FavoriteSchema
+    }
+  ]
 });
 
 // Execute before each user.save() call
