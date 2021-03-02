4e4fe1ad8956f136d2766f6d42568cc40ac435c2
Claudia Hernandez <claudiahdez13@gmail.com>
2021-03-02 15:00:34 -0800

    got the api working on the backend

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -1,15 +1,17 @@
 const { Router } = require("express");
 const jobsRouter = new Router();
-
+const axios = require("axios");
 
 jobsRouter.get("/api/job-listings", (req, res) => {
-    const { query: params } = req;
-        axios
-          .get("https://jobs.github.com/positions.json?description=remote", {
-            params
-          })
-          .then(Jobs => res.json(Jobs))
-          .catch(err => res.status(422).json(err));
-  });
+  const { query: params } = req;
+  axios
+    .get("https://jobs.github.com/positions.json", {
+      params,
+    })
+    .then((jobsResponse) => {
+      res.json(jobsResponse.data);
+    })
+    .catch((err) => res.status(422).json(err));
+});
 
-  module.exports = jobsRouter;
+module.exports = jobsRouter;
