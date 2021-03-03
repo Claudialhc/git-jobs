8f0a2d982c1215eb982e8b022ad1864d2763187b
Claudia Hernandez <claudiahdez13@gmail.com>
2021-03-03 11:10:05 -0800

    protected page styling update

--- client/src/pages/ProtectedPage.js
+++ client/src/pages/ProtectedPage.js
@@ -1,12 +1,14 @@
 import { useAuth } from "../util/authContext";
 import { Jumbotron, Container } from "react-bootstrap";
+import SignUpPage from "./SignUpPage";
+
 function ProtectedPage() {
   const auth = useAuth();
   return (
     <div>
       <Jumbotron fluid>
         <Container>
-          <h1>Welcome (username)</h1>
+          <h1>Welcome to GitJobs!</h1>
         </Container>
       </Jumbotron>
       <h2>Protected</h2>
