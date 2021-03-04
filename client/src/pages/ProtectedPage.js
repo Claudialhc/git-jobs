1e50b7b0b053f7a1ff0be4f341663f380fe84f43
Timothy Smith <timothytsmith8@gmail.com>
2021-03-04 12:31:24 -0800

    Delete button is now functional. And lots of clean up in code.

--- client/src/pages/ProtectedPage.js
+++ client/src/pages/ProtectedPage.js
@@ -1,38 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../util/authContext";
 import { Jumbotron, Container } from "react-bootstrap";
-import JobCard from "../components/JobCard";
+import FavoriteJobCard from "../components/FavoriteJobCard";
 import API from "../util/API.js";
 
 function ProtectedPage() {
   const [jobs, setJobs] = useState([]);
+  const auth = useAuth();
+  
   useEffect(() => {
     API.getUser().then((response) => {
       setJobs(response.data.favorites);
-
-      // (response.data);
     });
   }, []);
 
-  const auth = useAuth();
 
-const logOut = () => {
-  auth.logout();
-  
-}
+  const deleteFavorite = (favoriteId) => {
+    API.deleteFavorite(favoriteId)
+      .then((res) => {
+        // sorry about the repetivie code. look into using the useCallback hook
+        // to factor out a loadJobs function
+        return API.getUser()
+      }).then((response) => {
+        setJobs(response.data.favorites);
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div>
       <Jumbotron fluid className="bg-info text-white">
         <Container>
           <h1 className="text-center">Welcome {auth.user.email}!</h1>
-          <div className= "text-center pt-3">
-          <button type="button" className="btn btn-success"  onClick={logOut}>Log Out</button>
+          <div className="text-center pt-3">
+            <button type="button" className="btn btn-success" onClick={() => auth.logout()}>
+              Log Out
+            </button>
           </div>
         </Container>
       </Jumbotron>
       {jobs.map((job) => {
-        return <JobCard key={job.id} job={job} />;
+        return <FavoriteJobCard key={job._id} job={job} onDeleteFavorite={deleteFavorite} />;
       })}
     </div>
   );
